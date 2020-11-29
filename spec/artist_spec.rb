5a63bd0ba17cd0b77dd9111efd7797b2047f1a4a
Arynn Boniface <arynn.boniface@gmail.com>
2020-11-29 10:44:15 -0500

    Done.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe "Artist" do
 
   let!(:adele) { Artist.new("Adele") }
+  let!(:drake) { Artist.new("Drake")}
 
     describe "#new" do
       it "is initialized with a name" do
@@ -21,6 +22,8 @@ describe "Artist" do
         expect(adele.songs).to be_a(Array)
         hello = Song.new("Hello")
         hello.artist = adele
+        hotline_bling = Song.new("Hotline Bling")
+        hotline_bling.artist = drake
         expect(adele.songs).to eq([hello])
       end
     end
